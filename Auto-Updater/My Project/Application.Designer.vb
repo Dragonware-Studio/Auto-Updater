acf5dc076881f7a7c9259a52b3dd5a456cdb5ebf
Jeramey Guay <jerameyg@gmail.com>
2023-03-19 20:39:04 -0400

    Changed default Namespace

--- Auto-Updater/My Project/Application.Designer.vb
+++ Auto-Updater/My Project/Application.Designer.vb
@@ -33,7 +33,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Auto_Updater.Form1
+            Me.MainForm = Global.DragonwareUpdater.Form1
         End Sub
     End Class
 End Namespace
